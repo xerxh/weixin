c582fd930ffe259e16dd2f8b7520454432b6033d
xerxh <36349120+xerxh@users.noreply.github.com>
2018-08-08 18:32:01 +0800

    Add files via upload

---
+++ exercise/pages/index/index.js
@@ -0,0 +1,71 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    motto: 'Hello World',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse){
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  },
+  onReady: function () {
+    // 获取dialog组件
+    this.dialog = this.selectComponent('#dialog')
+  },
+  showDialog () {
+    this.dialog.showDialog();
+  },
+  // 取消事件
+  _cancelEvent () {
+    console.log('取消');
+    this.dialog.hideDialog();
+  },
+  // 确认事件
+  _confirmEvent () {
+    console.log('确认');
+    this.dialog.hideDialog();
+  }
+})
