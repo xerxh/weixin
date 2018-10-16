7a424b013d3d2859bd9df413464abe68941d4338
xerxh <xhxer512@163.com>
2018-10-16 15:40:44 +0800

    promise封装

--- exercise/utils/http.js
+++ exercise/utils/http.js
@@ -7,42 +7,54 @@ class HTTP {
     this.baseRestUrl = config.api_blink_url
   }
 
+  request({url, data = {}, method = "GET"}) {
+    return this._request(url, data, method)
+  }
+
   //http 请求类
-  request(params) {
-    var that = this
-    // api接口地址
-    var url = this.baseRestUrl + params.url;
-    // 如果没有指定方法  默认使用GET请求
-    if (!params.method) {
-      params.method = 'GET';
-    }
-    // wx提供的ajax请求接口
-    wx.request({
-      url: url,
-      data: params.data,
-      method: params.method,
-      header: {
-        'content-type': 'application/json'
-      },
-      success: function (res) {
-        // 判断以2（2xx)开头的状态码为正确
-        // 异常不要返回到回调中，就在request中处理，记录日志并showToast一个统一的错误即可
-        var code = res.statusCode.toString();
-        var startChar = code.charAt(0);
-        // 如果请求返回状态为2开头
-        if (startChar == '2') {
-          // 成功处理方法
-          params.success && params.success(res.data);
-        } else {
-          // 错误码处理方法
-          params.error && params.error(res);
+  _request(url, data, method) {
+   const _this = this  
+   return new Promise((resolve, reject) => {
+      // api接口地址
+      var url = this.baseRestUrl + url;
+      // wx提供的ajax请求接口
+      wx.request({
+        url: url,
+        data: data,
+        method: method,
+        header: {
+          'content-type': 'application/json'
+        },
+        success: function (res) {
+          // 判断以2（2xx)开头的状态码为正确
+          // 异常不要返回到回调中，就在request中处理，记录日志并showToast一个统一的错误即可
+          var code = res.statusCode.toString();
+          var startChar = code.charAt(0);
+          // 如果请求返回状态为2开头
+          if (startChar == '2') {
+            // 成功处理方法
+            resolve(res)
+          } else {
+            // 错误码处理方法
+            _this.showError(code)
+            reject()
+          }
+        },
+        fail: function (err) {
+          // 錯誤处理
+          _this.showError()
+          reject()
         }
-      },
-      fail: function (err) {
-        // 如果有错误处理方法 则执行  没有则不执行
-        params.fail && params.fail(err)
-      }
-    });
+      });
+   })
+  }
+
+  showError(code) {
+    console.log(config.error_code[code] || config.error_code[0])
+    wx.showToast({
+      title: config.error_code[code] || config.error_code[0],
+      icon: 'none'
+    })
   }
 };
 
