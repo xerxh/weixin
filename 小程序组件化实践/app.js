8a9a70334d0772d7fac9ea9b89a90ab2d0a1a4d5
xerxh <36349120+xerxh@users.noreply.github.com>
2020-01-08 22:56:54 +0800

    Update app.js

--- 小程序组件化实践/app.js
+++ 小程序组件化实践/app.js
@@ -9,6 +9,7 @@ App({
     wx.setStorageSync('logs', logs)
     if(!wx.getStorageSync('userInfo')){
       // 登录
+      
       wx.login({
         success: res => {
           // 发送 res.code 到后台换取 openId, sessionKey, unionId
